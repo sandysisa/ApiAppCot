6d8f02955bddbf37ea34abaded10363d2a81ce8b
sandysisa <sandy.sisa@gmail.com>
2022-10-30 09:57:01 -0300

    nullable nome

--- Models/Ativos.cs
+++ Models/Ativos.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int fk_idfavoritos { get; set; }
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public double valor_alvo { get; set; }
         public bool Alarme { get; set; }
     }
